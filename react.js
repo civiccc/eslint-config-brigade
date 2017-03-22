60d3d81bc24525f83b1eb8ab286d009aa0395c72
Matthew Herbst <matt.herbst@brigade.com>
2017-03-22 16:38:46 -0700

    Remove duplicate rules
    This removes rules that are present in the recommended configs
    and in our rule sets. A couple of rules were also updated to their
    newer versions since they were deprecated.

--- react.js
+++ react.js
@@ -15,7 +15,6 @@ module.exports = {
   },
 
   'rules': {
-    'react/display-name': 0,
     'react/forbid-prop-types': 0,
     'react/jsx-boolean-value': [2, 'never'],
     'react/jsx-closing-bracket-location': [1, 'tag-aligned'],
@@ -24,17 +23,11 @@ module.exports = {
     'react/jsx-max-props-per-line': 0,
     'react/jsx-no-duplicate-props': [2, { ignoreCase: true }],
     'react/jsx-no-literals': 0,
-    'react/jsx-no-undef': 2,
     'react/jsx-pascal-case': 2,
-    'react/jsx-uses-react': 1,
-    'react/jsx-uses-vars': 1,
     'react/jsx-wrap-multilines': 2,
     'react/no-did-mount-set-state': 1,
     'react/no-did-update-set-state': 1,
     'react/no-multi-comp': 1,
-    'react/no-unknown-property': 2,
-    'react/prop-types': 2,
-    'react/react-in-jsx-scope': 1,
     'react/self-closing-comp': 2,
     'react/sort-comp': 2,
   }
