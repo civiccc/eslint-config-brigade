06cacede01ee3b6a8b96811ec53dfdff0675aa67
Matthew Herbst <matt.herbst@brigade.com>
2017-12-21 11:15:05 -0800

    Add rule: no-control-regex
    We don't use controlcharacter pattern matching. If you need this,
    disable the rule locally.

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ module.exports = {
     'no-cond-assign': 2,
     'no-confusing-arrow': [2,  { allowParens: true }],
     'no-constant-condition': 2,
+    'no-control-regex': 2,
     'no-duplicate-imports': 2,
     'no-eq-null': 2,
     'no-eval': 2,
